f243c3afea57c07dd4732a54bccadb867b820dad
Gonçalo Marques <goncalomarques101@gmail.com>
2018-01-11 11:59:55 +0000

    better structure

--- src/views/app/app.vue
+++ src/views/app/app.vue
@@ -1,21 +1,11 @@
 <template>
-  <div>
-    <tsl-map />
-    <tsl-header />
-    <router-view/>
-  </div>
+  <router-view/>
 </template>
 
 <script>
-import TslHeader from '@/components/tsl-header/tsl-header';
-import TslMap from '@/components/tsl-map/tsl-map';
 
 export default {
-  name: 'App',
-  components: {
-    TslHeader,
-    TslMap
-  }
+  name: 'App'
 }
 </script>
 <style lang="scss">
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,67 +1,14 @@
 <template>
-  <div
-    id="home"
-    class="container">
-    <div v-if="!fetchedEmployees">
-      <tsl-button
-        id="fetch-employees"
-        title="Get the Tesselo Squad!!"
-        @click="getEmployees"
-      />
-    </div>
-    <div
-      v-if="fetchedEmployees"
-    >
-      <h1>Tesselo squad</h1>
-      <div class="d-flex flex-row justify-content-start flex-wrap">
-        <div
-          v-for="employee in employees"
-          :key="employee.profile.image_192"
-          class="employee d-flex flex-row">
-          <img
-            :src="employee.profile.image_192"
-            :alt="employee.profile.real_name_normalized">
-          <div class="d-flex flex-column justify-content-start">
-            <h2>{{ employee.profile.real_name_normalized }}</h2>
-            <h3>{{ employee.profile.title }}</h3>
-          </div>
-        </div>
-      </div>
-    </div>
-
-  </div>
+  <tsl-map />
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex'
-import TslButton from '@/components/tsl-button/tsl-button';
-import { EMPLOYEES_GET_EMPLOYEES } from '@/services/constants/action-types'
+import TslMap from '@/components/tsl-map/tsl-map'
 
 export default {
   name: 'Home',
   components: {
-    TslButton
-  },
-  data () {
-    return {
-      fetchedEmployees: false
-    }
-  },
-  computed: {
-    ...mapState({
-      employees: state => state.employees.employees
-    })
-  },
-  methods: {
-    ...mapActions('employees', {
-      getEmployeesAction: EMPLOYEES_GET_EMPLOYEES
-    }),
-    getEmployees () {
-      return this.getEmployeesAction()
-        .then(() => {
-          this.fetchedEmployees = true;
-        });
-    }
+    TslMap
   },
   head: {
     title: {
@@ -70,37 +17,5 @@ export default {
   }
 }
 </script>
-
 <style lang="scss" scoped>
-
-h1 {
-  font-size: 22px;
-  line-height: 32px;
-  color: $tsl-grey;
-  font-weight: 500;
-  overflow: hidden;
-}
-
-.employee {
-  width: 25%;
-  margin-top: 40px;
-
-  img {
-    width: 80px;
-    height: 80px;
-    border-radius: 50%;
-  }
-
-  h1,
-  h2 {
-    margin-top: 10px;
-    margin-left: 15px;
-    font-size: 16px;
-  }
-
-  h3 {
-    margin-left: 15px;
-    font-size: 14px;
-  }
-}
 </style>
