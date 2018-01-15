634ab4eef0ebff1cd3530819751026dfd0012539
Gonçalo Marques <goncalomarques101@gmail.com>
2018-01-15 15:14:09 +0000

    wip error handling

--- src/services/state/auth/store/auth.actions.js
+++ src/services/state/auth/store/auth.actions.js
@@ -10,10 +10,28 @@ export default {
    * @param {any} context
    */
   [actionTypes.AUTH_LOGIN] (context, user) {
-    return APIAdapter.services.auth.login(user)
-      .then(() => {
-        context.commit(mutationTypes.AUTH_SET_AUTHENTICATION, true)
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        // resolve({
+        //   username: user.username,
+        //   token: 'REPLACE TOKEN ON REQUEST IMPLEMENTATION',
+        //   expires: 'REPLACE EXPIRES ON REQUEST IMPLEMENTATION'
+        // })
+        reject({
+          "non_field_errors": [
+              "Unable to log in with provided credentials."
+          ]
       })
+      }, 2000)
+    })
+    .then((response) => {
+      context.commit(mutationTypes.AUTH_SET_AUTHENTICATION, response)
+    })
+
+    // return APIAdapter.services.auth.login(user)
+    //   .then(() => {
+    //     context.commit(mutationTypes.AUTH_SET_AUTHENTICATION, true)
+    //   })
   },
 
   /**
