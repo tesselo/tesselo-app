6ee6aedea41c587d22358127a5aff7aff7e00d3d
Gonçalo Marques <goncalomarques101@gmail.com>
2018-01-17 09:58:47 +0000

    wip panel-selector

---
+++ src/components/panel-selector/panel-selector.vue
@@ -0,0 +1,65 @@
+<template>
+  <a
+    class="panel-selector d-flex flex-row justify-content-start align-items-center"
+    @click="$emit('click')">
+    <img 
+      class="panel-selector-icon"
+      :src="`/static/icons/${icon}.svg`">
+    <span class="panel-selector-title">{{ title }}</span>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'PanelSelector',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    active: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    selected: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .panel-selector {
+    display: block;
+    height: 40px;
+    width: 155px;
+    margin-top: 2px;
+
+    border-radius: 2px;
+    background-color: white;
+    cursor: pointer;
+
+
+    &:hover{
+      background-color: darken(white, 5%);
+    }
+  }
+
+  .panel-selector-icon {
+    
+  }
+
+  .panel-selector-title {
+    font-size: 12px;
+    line-height: 17px;
+    color: $twilight-blue;
+  }
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -4,6 +4,17 @@
       class="logout-button"
       title="Logout"
       @click="logout" />
+    <div class="menu">
+      <panel-selector
+        title="Areas"
+        @click="openAreas"
+        icon="crosshair" />
+      <panel-selector 
+        title="Layers"
+        @click="openLayers"
+        icon="layers" />
+    </div>
+    <div class="panel"></div>
     <tsl-map />
   </div>
 </template>
@@ -14,12 +25,14 @@ import { actionTypes } from '@/services/constants'
 
 import TslMap from '@/components/tsl-map/tsl-map'
 import TslButton from '@/components/tsl-button/tsl-button'
+import PanelSelector from '@/components/panel-selector/panel-selector'
 
 export default {
   name: 'Home',
   components: {
     TslMap,
-    TslButton
+    TslButton,
+    PanelSelector
   },
   head: {
     title: {
@@ -29,16 +42,28 @@ export default {
   methods: {
     ...mapActions('auth', {
       logout: actionTypes.AUTH_LOGOUT
-    })
+    }),
+    openAreas() {
+      console.log('olá')
+    },
+    openLayers() {
+      console.log('olé')
+    }
   }
 }
 </script>
 <style lang="scss" scoped>
-
   .logout-button {
     position: absolute;
     top: 20px;
-    left: 20px;
+    left: 25px;
+    z-index: z('content');
+  }
+
+  .menu {
+    position: absolute;
+    top: 60px;
+    left: 25px;
     z-index: z('content');
   }
 </style>
