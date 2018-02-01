34c580bd43de568365e182bbfd5620cc143f4fd0
Gonçalo Marques <goncalomarques101@gmail.com>
2018-02-01 10:08:26 +0000

    remove empty lines

--- src/services/util.js
+++ src/services/util.js
@@ -30,8 +30,6 @@ export const capitalizeFirstLetter = function(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-
-
 const sentinelBands = [{
   rasterlayerName: 'B01.jp2',
   search: 'B1',
