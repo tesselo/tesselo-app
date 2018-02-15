2d654ec84a1883c82ca2f128401a085b7f442016
Gonçalo Marques <goncalomarques101@gmail.com>
2018-02-15 16:01:27 +0000

    add router base for production

--- src/services/router/index.js
+++ src/services/router/index.js
@@ -8,6 +8,7 @@ import Logout from '@/views/logout/logout'
 Vue.use(Router)
 
 const router = new Router({
+  base: process.env.NODE_ENV === 'production' ? '/mosaic' : '/',
   mode: 'history',
   routes: [
     {
