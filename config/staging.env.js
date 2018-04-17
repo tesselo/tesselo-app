f99cad9db734f661b50a59281bdd78ffdb33ca9f
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2018-04-17 14:01:11 +0100

    Forced production build for staging environment.

--- config/staging.env.js
+++ config/staging.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"staging"',
+  NODE_ENV: '"production"',
   API_URL: '"https://staging.tesselo.com/api/"',
   ROUTER_BASE: '"/app"',
   ASSETS_PUBLIC_PATH: '"https://stagingstatic.tesselo.com/"'
