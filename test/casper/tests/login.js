a95a5253594687d8c803a3633bac9ebd3523eefd
Daniel Wiesmann <daniel@tesselo.com>
2020-01-13 13:13:49 +0000

    Added casper based e2e tests.

---
+++ test/casper/tests/login.js
@@ -0,0 +1,46 @@
+const setup = require('../setup')
+
+// Get authentication credentials from command line args.
+const creds = setup.get_creds(casper);
+const has_creds = creds.username && creds.password;
+
+casper.test.begin('Tesselo login form works (currently mainly disabled)', 1, function suite(test) {
+
+    casper.start(setup.get_base_url(casper) + "login", function() {
+
+      // Skip these tests if creds are not available.
+      casper.thenBypassIf(function(){ return !has_creds }, 5);
+
+      casper.then(function() {
+        casper.fill('form', {
+          'username': creds.username,
+          'password': creds.password
+        }, false);
+
+        console.log('username', this.evaluate(function() {
+          return document.querySelector('input[name="username"]').value
+        }));
+
+        console.log('password', this.evaluate(function() {
+          return document.querySelector('input[name="password"]').value
+        }));
+        casper.click('.login-button');
+        casper.sendKeys('.login-button', casper.page.event.key.Enter, {keepFocus: true});
+      });
+
+      casper.wait(500);
+
+      casper.then(function() {
+        // console.log(casper.evaluate(function(){
+        //   return document.body.innerHTML;
+        // }));
+        // This title is not the one after the login. The form submission fails
+        // in this casperjs framework (bug).
+        test.assertTitle("Tesselo | Tesselo", "Tesselo title is the one expected.");
+      });
+    });
+
+    casper.run(function() {
+        test.done();
+    });
+});
