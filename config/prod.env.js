5d8ec349cc27103ad7cb20f0ee0f7de4d94b7f19
Daniel Wiesmann <daniel@tesselo.com>
2020-01-13 11:29:57 +0000

    Updated app build configs to use new api endpoints.

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"https://tesselo.com/api/"',
-  ROUTER_BASE: '"/app"',
-  ASSETS_PUBLIC_PATH: '"https://static.tesselo.com/"'
+  API_URL: '"https://api.tesselo.com/"',
+  ROUTER_BASE: '""',
+  ASSETS_PUBLIC_PATH: '""'
 }
--- config/staging.env.js
+++ config/staging.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_URL: '"https://staging.tesselo.com/api/"',
-  ROUTER_BASE: '"/app"',
-  ASSETS_PUBLIC_PATH: '"https://stagingstatic.tesselo.com/"'
+  API_URL: '"https://stagingapi.tesselo.com/"',
+  ROUTER_BASE: '""',
+  ASSETS_PUBLIC_PATH: '""'
 }
