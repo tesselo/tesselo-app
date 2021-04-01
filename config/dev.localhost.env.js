97ff904d4c4131d4713a52e09bc43d5dd0a64790
Daniel Wiesmann <daniel@tesselo.com>
2021-04-01 16:06:48 +0100

    Added stagingapi as option to run the dev server.

--- config/dev.localhost.env.js
+++ config/dev.localhost.env.js
@@ -3,5 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./dev.env')
 
 module.exports = merge(prodEnv, {
-  API_URL: '"http://localhost/"',
+  API_URL: process.env.API_ENV == 'staging' ? '"https://stagingapi.tesselo.com/"' : '"http://localhost/"',
 })
