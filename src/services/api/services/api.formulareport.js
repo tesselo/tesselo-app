41aaacc1f326dca4d56c9f3c823f8aa6540c88d8
Vitor Paixão <vitornvpaixao@gmail.com>
2021-04-13 18:34:50 +0100

    Fix: Reversed linter-related changes in code

--- src/services/api/services/api.formulareport.js
+++ src/services/api/services/api.formulareport.js
@@ -1,44 +1,25 @@
-import endpoints from "@/services/api/api.endpoints";
-import client from "@/services/api/api.client";
+import endpoints from '@/services/api/api.endpoints'
+import client from '@/services/api/api.client'
 
 export default {
-  get(
-    {
-      layer,
-      formula,
-      moment,
-      predictedLayer,
-      ordering,
-      search,
-      dateAfter,
-      dateBefore,
-      page,
-      pageSize,
-      minPercentageCovered,
-      aggregationArea,
-    },
-    axiosInstance = client
-  ) {
-    return axiosInstance[endpoints.formulaReport.getFormulaReport.method](
-      endpoints.formulaReport.getFormulaReport.url,
-      {
-        params: {
-          formula: formula ? formula.id : "",
-          aggregationarea: aggregationArea || "",
-          aggregationlayer: layer.id,
-          composite: moment ? moment.id : "",
-          predictedlayer: predictedLayer ? predictedLayer.id : "",
-          search: search,
-          ordering: ordering,
-          date_after: dateAfter,
-          date_before: dateBefore,
-          page: page,
-          page_size: pageSize,
-          min_stats_percentage_covered: minPercentageCovered,
-        },
+  get({ layer, formula, moment, predictedLayer, ordering, search, dateAfter, dateBefore, page, pageSize, minPercentageCovered }, axiosInstance = client) {
+    return axiosInstance[endpoints.formulaReport.getFormulaReport.method](endpoints.formulaReport.getFormulaReport.url, {
+      params: {
+        formula: formula ? formula.id : '',
+        aggregationlayer: layer.id,
+        composite: moment ? moment.id : '',
+        predictedlayer: predictedLayer ? predictedLayer.id : '',
+        search: search,
+        ordering: ordering,
+        date_after: dateAfter,
+        date_before: dateBefore,
+        page: page,
+        page_size: pageSize,
+        min_stats_percentage_covered: minPercentageCovered,
       }
-    ).then((response) => {
-      return Promise.resolve(response.data);
-    });
+    })
+    .then((response) => {
+      return Promise.resolve(response.data)
+    })
   },
-};
+}
