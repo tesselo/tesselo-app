4a303d9bcf7e90ac1f06a8bcfadb672a8f744e1b
Vitor Paixão <vitornvpaixao@gmail.com>
2021-05-12 18:04:50 +0100

    Feature: - Added new component to show notifications with attribute table;

---
+++ src/components/notification/src/main.js
@@ -0,0 +1,50 @@
+import Vue from 'vue';
+import Main from './main.vue';
+import merge from 'element-ui/src/utils/merge';
+import { PopupManager } from 'element-ui/src/utils/popup';
+import { isVNode } from 'element-ui/src/utils/vdom';
+import _ from 'lodash';
+const NotificationConstructor = Vue.extend(Main);
+
+let instance;
+let instances = [];
+
+const Notification = function(options) {
+  if (Vue.prototype.$isServer) return;
+  options = merge({}, options);
+
+  options.onClose = function() {
+    Notification.close();
+  };
+
+  instance = new NotificationConstructor({
+    data: options
+  });
+
+  if (isVNode(options.buttonName)) {
+    instance.$slots.default = [options.buttonName];
+    options.buttonName = 'REPLACED_BY_VNODE';
+  }
+
+  instance.$mount();
+  document.body.appendChild(instance.$el);
+  instance.visible = true;
+  instance.dom = instance.$el;
+  instance.dom.style.zIndex = PopupManager.nextZIndex();
+
+  let verticalOffset = options.offset || 0;
+   verticalOffset += 16;
+   instance.verticalOffset = verticalOffset;
+   instances.push(instance);
+  return instance;
+};
+
+Notification.close = function() {
+  instances.splice(0, 1);
+};
+
+Notification.closeAll = function() {
+    if (!_.isEmpty(instances)) instances[0].close();
+};
+
+export default Notification;
