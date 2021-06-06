1fb79acfbb6931953b8e0b7be8c40c01d401468a
Vitor Paixão <vitornvpaixao@gmail.com>
2021-06-06 12:54:51 +0100

    Update: - Added new call to get and set data to create report area graph for time series by year; - Added new logic to process data for graph; - Updated state to save graph data;

--- src/services/state/formula-report/store/formula-report.actions.js
+++ src/services/state/formula-report/store/formula-report.actions.js
@@ -19,5 +19,37 @@ export default {
         }
       )
     })
+  },
+
+  /**
+   *
+   * GET GRAPH INFO FOR REPORT AREA
+   */
+  [actionTypes.FORMULA_REPORT_CHART_GET](context, { layer, formula, moment, predictedLayer, ordering, search, dateAfter, dateBefore, page, pageSize, minPercentageCovered, aggregationArea }) {
+    return APIAdapter.services.formulaReport.get({ layer, formula, moment, predictedLayer, ordering, search, dateAfter, dateBefore, page, pageSize, minPercentageCovered, aggregationArea })
+      .then((response) => {
+        // Function to convert month number in month name
+        function monthName(mon) {
+          return ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][mon - 1];
+        }
+
+        // Create new array only with graph info needed
+        const chartData = response.results.map((entry) => {
+          return {
+            aggregationarea: entry['aggregationarea'],
+            avg: entry['avg'] ? Number(entry['avg'].toFixed(2)) : null,
+            year: entry['max_date'].slice(0,4),
+            month: monthName(Number(entry['max_date'].slice(5,7))),
+          }
+        })
+
+        // Create new array with available years for chart
+        const chartYears = chartData.reduce((acc, current) => {
+          const x = acc.find(item => item === current.year)
+            return !x ? acc.concat([current.year]) : acc
+          },[]).sort()
+
+        context.commit(mutationTypes.FORMULA_REPORT_CHART_SET, { chartData, chartYears })
+    })
   }
 }
