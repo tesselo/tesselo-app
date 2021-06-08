513fbbd394b02362e1fcce250081453a20d358b7
Vitor Paixão <vitornvpaixao@gmail.com>
2021-06-08 17:58:08 +0100

    Updated Assets folder with two new files: - Added two new support files (assets/utils/..), one for months and other for chart colors; - Updated path in places that used the months; - Removed supported file with months in selector time dimension component;

---
+++ src/assets/utils/months.js
@@ -0,0 +1,50 @@
+export default [
+    {
+        label: 'Jan',
+        completed: 'January'
+    },
+    {
+        label: 'Feb',
+        completed: 'February'
+    },
+    {
+        label: 'Mar',
+        completed: 'March'
+    },
+    {
+        label: 'Apr',
+        completed: 'April'
+    },
+    {
+        label: 'May',
+        completed: 'May'
+    },
+    {
+        label: 'Jun',
+        completed: 'June'
+    },
+    {
+        label: 'Jul',
+        completed: 'July'
+    },
+    {
+        label: 'Aug',
+        completed: 'August'
+    },
+    {
+        label: 'Sep',
+        completed: 'September'
+    },
+    {
+        label: 'Oct',
+        completed: 'October'
+    },
+    {
+        label: 'Nov',
+        completed: 'November'
+    },
+    {
+        label: 'Dec',
+        completed: 'December'
+    }
+]
