3f3951fdce9529825a74253722a0e689480e9b77
Vitor Paixão <vitornvpaixao@gmail.com>
2021-07-21 15:32:16 +0100

    feat(formulasAPI): add new query parameter and sentry message

--- src/services/api/services/api.formula.js
+++ src/services/api/services/api.formula.js
@@ -1,6 +1,7 @@
 import endpoints from '@/services/api/api.endpoints'
 import client from '@/services/api/api.client'
 import { formatFormulaResults, formatFormulaResult } from '@/services/api/api.models'
+import * as Sentry from "@sentry/vue"
 
 export default {
 
@@ -9,12 +10,24 @@ export default {
    *
    * @param {any} [axiosInstance=client]
    * @param {Object} options
+   * @param {Number} options.pageSize
    * @param {Number} options.page
-   * @param {String} options.search
    * @returns
    */
   get (options, axiosInstance = client) {
-    return axiosInstance[endpoints.formula.list.method](endpoints.formula.list.url + '?page=' + options.page)
+    const optionKey = Object.keys(options)[0]
+    const optionValue = options[optionKey]
+    let queryParam
+
+    if (options.page) {
+      queryParam = 'page'
+    } else if (options.pageSize) {
+      queryParam = 'page_size'
+    } else {
+      Sentry.captureMessage(`Get Formulas API: query parameter "${queryParam}" is invalid.`)
+    }
+
+    return axiosInstance[endpoints.formula.list.method](endpoints.formula.list.url + `?${queryParam}=` + optionValue)
       .then((response) => {
         return Promise.resolve({
           ...response.data,
@@ -28,8 +41,6 @@ export default {
    *
    * @param {any} [axiosInstance=client]
    * @param {Number} id
-   * @param {Number} options.page
-   * @param {String} options.search
    * @returns
    */
   getById (id, axiosInstance = client) {
