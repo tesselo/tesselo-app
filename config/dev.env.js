c46760400f0b677bed602896d6eae86ca4be9a86
Vitor Paixão <vitornvpaixao@gmail.com>
2021-10-12 19:27:25 +0100

    chore(build-system-config): update webpack configuration

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,11 +1,11 @@
 'use strict'
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  API_URL: '"https://tesselo.com/api/"',
-  ROUTER_BASE: '"/"',
-  ASSETS_PUBLIC_PATH: '"/"',
-  SENTRY_DSN: '""'
+  NODE_ENV: 'development',
+  API_URL: 'https://tesselo.com/api/',
+  ROUTER_BASE: '/',
+  ASSETS_PUBLIC_PATH: '/',
+  SENTRY_DSN: ''
 })
