042d903b20f12c5a559cd966da7a990e09f26b11
Daniel Moreno <daniel.moreno@tesselo.com>
2021-11-11 18:17:08 +0000

    chore(test): delete unused tests

--- test/unit/setup.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import { mount, shallow, createLocalVue } from '@vue/test-utils'
-
-global.mount = mount
-global.shallow = shallow
-global.createLocalVue = createLocalVue
-
-Vue.config.productionTip = false
